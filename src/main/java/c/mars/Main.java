e290479fbffc8f460b03ef6268e3b970afdaa90a
Constantine Mars <constantine.mars@gmail.com>
2015-04-07 18:39:05 +0300

    test json is valid

--- src/main/java/c/mars/Main.java
+++ src/main/java/c/mars/Main.java
@@ -1,6 +1,11 @@
 package c.mars;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import jdk.nashorn.internal.parser.JSONParser;
+
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -16,6 +21,29 @@ public class Main {
         } else {
             System.out.println("r: "+r.getStatus());
         }
+
+        String[] jsons = {
+                json,
+                "a",
+                "{b:c}",
+                "{okey",
+                "{\"c\":\"d\"}"
+        };
+        for (String s:jsons) {
+            System.out.println(s + "->" + valid(s));
+        }
+//        JsonObject jsonObject = (JsonObject)new JSONParser().parse(json);
+    }
+
+    public static boolean valid(String json){
+        Gson gson = new Gson();
+        try {
+            Object o = gson.fromJson(json, Object.class);
+            String reversed = new GsonBuilder().setPrettyPrinting().create().toJson(o);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
     }
 
     private static class R {
